23a1f33535c420c795700f442a853bfe3f46962e
boxer <leebh0806@naver.com>
2025-08-19 11:26:07 +0900

    Initial Commit

--- temp.py
+++ temp.py
@@ -1,10 +1,1 @@
 
-my_int: int = 123 #Literal : 값
-my_str: str = "abc"
-
-my_list: list[str] = ["abc", "def"]
-
-my_tuple: tuple[str, str] = ("abc", "def")
-
-my_dict: dict[str, int] = {"a": 1, "b": 2}
-
